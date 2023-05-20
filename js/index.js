30c56979a4405b6bb840dea6ea7cb07d748dbbc4
João Ponchiroli <115267364+JPonchiroli@users.noreply.github.com>
2023-05-20 08:54:49 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,45 @@
+function rate1() {
+    document.getElementById('rate1').style = 'background-color: #a4a4a4;';
+    document.getElementById('rate2').style = 'background-color: #3b444d';
+    document.getElementById('rate3').style = 'background-color: #3b444d';
+    document.getElementById('rate4').style = 'background-color: #3b444d';
+    document.getElementById('rate5').style = 'background-color: #3b444d';
+}
+
+
+function rate2() {
+    rate = 2;
+    document.getElementById('rate1').style = 'background-color: #3b444d;';
+    document.getElementById('rate2').style = 'background-color: #a4a4a4';
+    document.getElementById('rate3').style = 'background-color: #3b444d';
+    document.getElementById('rate4').style = 'background-color: #3b444d';
+    document.getElementById('rate5').style = 'background-color: #3b444d';
+}
+
+function rate3() {
+    rate = 3;
+    document.getElementById('rate1').style = 'background-color: #3b444d;';
+    document.getElementById('rate2').style = 'background-color: #3b444d';
+    document.getElementById('rate3').style = 'background-color: #a4a4a4';
+    document.getElementById('rate4').style = 'background-color: #3b444d';
+    document.getElementById('rate5').style = 'background-color: #3b444d';
+}
+
+function rate4() {
+    rate = 4;
+    document.getElementById('rate1').style = 'background-color: #3b444d;';
+    document.getElementById('rate2').style = 'background-color: #3b444d';
+    document.getElementById('rate3').style = 'background-color: #3b444d';
+    document.getElementById('rate4').style = 'background-color: #a4a4a4';
+    document.getElementById('rate5').style = 'background-color: #3b444d';
+}
+
+function rate5() {
+    rate = 5;
+    document.getElementById('rate1').style = 'background-color: #3b444d;';
+    document.getElementById('rate2').style = 'background-color: #3b444d';
+    document.getElementById('rate3').style = 'background-color: #3b444d';
+    document.getElementById('rate4').style = 'background-color: #3b444d';
+    document.getElementById('rate5').style = 'background-color: #a4a4a4';
+}
+
